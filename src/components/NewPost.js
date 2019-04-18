10a47adf4149a36b7ce17bf764a16ae1d16da0ff
Lucas Henrique Procopio <lu-procopio@hotmail.com>
2019-04-18 06:36:42 -0300

    test: New comment
    Test of dispatching a new comment on post details

--- src/components/NewPost.js
+++ src/components/NewPost.js
@@ -1,61 +1,65 @@
-import React from 'react'
-import { connect } from 'react-redux'
-import { handleCategoryData, createNewPost } from '../actions/shared'
-import PostForm from './PostForm'
+import React from "react";
+import { connect } from "react-redux";
+import { handleCategoryData, createNewPost } from "../actions/shared";
+import PostForm from "./PostForm";
 
 class NewPost extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "",
+      author: "",
+      body: "",
+      category: "react"
+    };
 
-    constructor(props) {
-        super(props)
-        this.state = {
-            title: '',
-            author: '',
-            body: '',
-            category: 'react'
-        }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-        this.handleChange = this.handleChange.bind(this)
-        this.handleSubmit = this.handleSubmit.bind(this)
-    }
+  componentDidMount() {
+    this.props.dispatch(handleCategoryData());
+  }
 
-    componentDidMount() {
-        this.props.dispatch(handleCategoryData())
-    }
+  handleChange(event) {
+    this.setState({ [event.target.name]: event.target.value });
+  }
 
-    handleChange(event) {
-        this.setState({ [event.target.name]: event.target.value })
-    }
+  handleSubmit(event) {
+    event.preventDefault();
+    // todo: post validation.
+    // todo: display character count validation
+    const post = {
+      id: Math.random()
+        .toString(36)
+        .substr(2, 9),
+      timestamp: Date.now(),
+      title: this.state.title,
+      body: this.state.body,
+      author: this.state.author,
+      category: this.state.category
+    };
 
-    handleSubmit(event) {
-        event.preventDefault()
-        // todo: post validation.
-        // todo: display character count validation
-        const post = {
-            id: Math.random().toString(36).substr(2, 9),
-            timestamp: Date.now(),
-            title: this.state.title,
-            body: this.state.body,
-            author: this.state.author,
-            category: this.state.category
-        }
+    this.props.dispatch(createNewPost(post));
+  }
 
-        this.props.dispatch(createNewPost(post))
-    }
-
-    render() {
-        return (
-            <div>
-                <PostForm
-                    handleChange={this.handleChange}
-                    handleSubmit={this.handleSubmit}
-                    post={this.state}
-                    categories={this.props.categories} />
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <PostForm
+          handleChange={this.handleChange}
+          handleSubmit={this.handleSubmit}
+          post={this.state}
+          categories={this.props.categories}
+        />
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = state => {
-    return { categories: Object.keys(state.categories).map(key => state.categories[key]) }
-}
-export default connect(mapStateToProps)(NewPost)
+  return {
+    categories: Object.keys(state.categories).map(key => state.categories[key])
+  };
+};
+export default connect(mapStateToProps)(NewPost);
--- src/reducers/comments.js
+++ src/reducers/comments.js
@@ -1,55 +1,52 @@
 import {
-    FETCH_COMMENTS,
-    INCREMENT_COMMENT_SCORE,
-    DECREMENT_COMMENT_SCORE,
-    EDIT_COMMENT_BODY,
-    ADD_NEW_COMMENT
-} from '../actions/comments'
+  FETCH_COMMENTS,
+  INCREMENT_COMMENT_SCORE,
+  DECREMENT_COMMENT_SCORE,
+  EDIT_COMMENT_BODY,
+  ADD_NEW_COMMENT
+} from "../actions/comments";
 
 export default function comments(state = {}, action) {
-    switch (action.type) {
-        case FETCH_COMMENTS:
-            return {
-                ...Object.assign({}, [...action.comments])
-            }
-        case INCREMENT_COMMENT_SCORE:
-            const upVoteComment = Object.keys(state)
-            for (let i of upVoteComment) {
-                if (state[i].id === action.commentId) {
-                    state[i].voteScore++
-                }
-            }
-            return {
-                ...state
-            }
-        case DECREMENT_COMMENT_SCORE:
-            const downVoteComment = Object.keys(state)
-            for (let i of downVoteComment) {
-                if (state[i].id === action.commentId) {
-                    state[i].voteScore--
-                }
-            }
-            return {
-                ...state
-            }
-        case ADD_NEW_COMMENT:
-            const newComment = Object.assign(state, [...action.comment])
-            console.log('>>>>>NEW', newComment)
-            return {
-                newComment
-            }
-        case EDIT_COMMENT_BODY:
-            Object.keys(state).map(key =>
-                (state[key].id === action.comment.id)
-                    ? state[key] = action.comment
-                    : null
-            )
-            return {
-                ...state
-            }
-        default:
-            return state
-
-
-    }
+  switch (action.type) {
+    case FETCH_COMMENTS:
+      return {
+        ...Object.assign({}, [...action.comments])
+      };
+    case INCREMENT_COMMENT_SCORE:
+      const upVoteComment = Object.keys(state);
+      for (let i of upVoteComment) {
+        if (state[i].id === action.commentId) {
+          state[i].voteScore++;
+        }
+      }
+      return {
+        ...state
+      };
+    case DECREMENT_COMMENT_SCORE:
+      const downVoteComment = Object.keys(state);
+      for (let i of downVoteComment) {
+        if (state[i].id === action.commentId) {
+          state[i].voteScore--;
+        }
+      }
+      return {
+        ...state
+      };
+    case ADD_NEW_COMMENT:
+      Object.assign(state, { [Object.keys(state).length]: action.comment });
+      return {
+        ...state
+      };
+    case EDIT_COMMENT_BODY:
+      Object.keys(state).map(key =>
+        state[key].id === action.comment.id
+          ? (state[key] = action.comment)
+          : null
+      );
+      return {
+        ...state
+      };
+    default:
+      return state;
+  }
 }
